d6b1531024f8ac89ced053bc6f7fcff58dfd0995
Jörg Richter <jri@deepamehta.de>
2010-06-11 00:11:53 +0200

    1) Also topic of newly created types get timestamp. 2) Adapted to current DeepaMehta core version.

--- src/main/java/de/deepamehta/plugins/time/TimePlugin.java
+++ src/main/java/de/deepamehta/plugins/time/TimePlugin.java
@@ -1,9 +1,13 @@
 package de.deepamehta.plugins.time;
 
+import de.deepamehta.core.model.DataField;
 import de.deepamehta.core.model.Topic;
+import de.deepamehta.core.model.TopicType;
 import de.deepamehta.core.plugin.DeepaMehtaPlugin;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 
@@ -12,17 +16,17 @@ public class TimePlugin extends DeepaMehtaPlugin {
 
     private Logger logger = Logger.getLogger(getClass().getName());
 
-    // --- Client-Side ---
-
     @Override
-    public String getClientPlugin() {
-        return "dm3_time.js";
-    }
-
-    // --- Server-Side ---
-
-    @Override
-    public void preCreateHook(Topic topic) {
+    public void preCreateHook(Topic topic, Map<String, String> clientContext) {
+        logger.info("### Adding timestamp to topic " + topic);
+        //
+        if (topic.typeId.equals("Topic Type")) {
+            DataField timeCreatedField = new DataField("time_created").setIndexingMode("FULLTEXT_KEY");
+            DataField timeModifiedField = new DataField("time_modified").setIndexingMode("FULLTEXT_KEY");
+            ((TopicType) topic).addDataField(timeCreatedField);
+            ((TopicType) topic).addDataField(timeModifiedField);
+        }
+        //
         String time = String.valueOf(System.currentTimeMillis());
         topic.setProperty("time_created", time);
         topic.setProperty("time_modified", time);
@@ -37,12 +41,17 @@ public class TimePlugin extends DeepaMehtaPlugin {
     // ---
 
     @Override
-    public void provideData(Topic topic) {
-        logger.info("provideData invoked!");
+    public void provideDataHook(Topic topic) {
+        logger.info("provideDataHook invoked!");
         topic.setProperty("time_modified", getService().getTopicProperty(topic.id, "time_modified"));
     }
 
     // ---
+
+    @Override
+    public String getClientPlugin() {
+        return "dm3_time.js";
+    }
 
     @Override
     public int requiredDBModelVersion() {
