2ee928632c010671eb1fae9449e135c0c03275ee
Jörg Richter <jri@deepamehta.de>
2010-06-30 02:31:36 +0200

    Adapted to current version of DeepaMehta 3 Core.

--- src/main/java/de/deepamehta/plugins/time/TimePlugin.java
+++ src/main/java/de/deepamehta/plugins/time/TimePlugin.java
@@ -27,18 +27,20 @@ public class TimePlugin extends Plugin {
     @Override
     public void preCreateHook(Topic topic, Map<String, String> clientContext) {
         //
-        if (topic.typeId.equals("Topic Type")) {
-            // Add "time_created" and "time_modified" data fields to the topic type being created.
+        if (topic.typeUri.equals("http://www.deepamehta.de/core/topictype/TopicType")) {
+            // Add "Date Created" and "Date Modified" data fields to the topic type being created.
             // Note: Topic types created before the time plugin was activated get these fields through the initial
             // migration. See de.deepamehta.plugins.time.migrations.Migration1
             //
             // TODO: Avoid this code doubling by providing a "update type definition" facility.
             //
-            DataField timeCreatedField = new DataField("time_created");
+            DataField timeCreatedField = new DataField("Date Created");
+            timeCreatedField.setUri("http://www.deepamehta.de/core/property/DateCreated");
             timeCreatedField.setDataType("number");
             timeCreatedField.setIndexingMode("FULLTEXT_KEY");
             //
-            DataField timeModifiedField = new DataField("time_modified");
+            DataField timeModifiedField = new DataField("Date Modified");
+            timeModifiedField.setUri("http://www.deepamehta.de/core/property/DateModified");
             timeModifiedField.setDataType("number");
             timeModifiedField.setIndexingMode("FULLTEXT_KEY");
             //
@@ -49,20 +51,21 @@ public class TimePlugin extends Plugin {
         // add a timestamp to the topic being created
         logger.info("Adding timestamp to " + topic);
         long time = System.currentTimeMillis();
-        topic.setProperty("time_created", time);
-        topic.setProperty("time_modified", time);
+        topic.setProperty("http://www.deepamehta.de/core/property/DateCreated", time);
+        topic.setProperty("http://www.deepamehta.de/core/property/DateModified", time);
     }
 
     @Override
     public void preUpdateHook(Topic topic) {
         long time = System.currentTimeMillis();
-        topic.setProperty("time_modified", time);
+        topic.setProperty("http://www.deepamehta.de/core/property/DateModified", time);
     }
 
     // ---
 
     @Override
     public void providePropertiesHook(Topic topic) {
-        topic.setProperty("time_modified", dms.getTopicProperty(topic.id, "time_modified"));
+        topic.setProperty("http://www.deepamehta.de/core/property/DateModified",
+            dms.getTopicProperty(topic.id, "http://www.deepamehta.de/core/property/DateModified"));
     }
 }
--- src/main/java/de/deepamehta/plugins/time/migrations/Migration1.java
+++ src/main/java/de/deepamehta/plugins/time/migrations/Migration1.java
@@ -10,23 +10,25 @@ public class Migration1 extends Migration {
 
     @Override
     public void run() {
-        // Add "time_created" and "time_modified" data fields to all existing topic types.
+        // Add "Date Created" and "Date Modified" data fields to all existing topic types.
         // Note: Topic types created after the time plugin is activated get these fields through the preCreateHook().
         // See de.deepamehta.plugins.time.TimePlugin
         //
         // TODO: Avoid this code doubling by providing a "update type definition" facility.
         //
-        DataField timeCreatedField = new DataField("time_created");
+        DataField timeCreatedField = new DataField("Date Created");
+        timeCreatedField.setUri("http://www.deepamehta.de/core/property/DateCreated");
         timeCreatedField.setDataType("number");
         timeCreatedField.setIndexingMode("FULLTEXT_KEY");
         //
-        DataField timeModifiedField = new DataField("time_modified");
+        DataField timeModifiedField = new DataField("Date Modified");
+        timeModifiedField.setUri("http://www.deepamehta.de/core/property/DateModified");
         timeModifiedField.setDataType("number");
         timeModifiedField.setIndexingMode("FULLTEXT_KEY");
         //
-        for (String typeId : dms.getTopicTypeIds()) {
-            dms.addDataField(typeId, timeCreatedField);
-            dms.addDataField(typeId, timeModifiedField);
+        for (String typeUri : dms.getTopicTypeUris()) {
+            dms.addDataField(typeUri, timeCreatedField);
+            dms.addDataField(typeUri, timeModifiedField);
         }
     }
 }
