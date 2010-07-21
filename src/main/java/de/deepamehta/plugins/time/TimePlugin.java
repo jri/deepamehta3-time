dbace3089356b1a5c94b9be662cb181897f07a5e
Jörg Richter <jri@deepamehta.de>
2010-07-21 16:06:27 +0200

    Bug fix: creating topics works also if timestamp data fields are interactively removed from a topic type meanwhile.

--- src/main/java/de/deepamehta/plugins/time/TimePlugin.java
+++ src/main/java/de/deepamehta/plugins/time/TimePlugin.java
@@ -14,6 +14,28 @@ import java.util.logging.Logger;
 
 public class TimePlugin extends Plugin {
 
+    // -------------------------------------------------------------------------------------------------- Static Methods
+
+    public static DataField createDateCreatedField() {
+        DataField dateCreatedField = new DataField("Date Created", "number");
+        dateCreatedField.setUri("de/deepamehta/core/property/DateCreated");
+        // dateCreatedField.setReadOnly(true);
+        dateCreatedField.setIndexingMode("FULLTEXT_KEY");
+        dateCreatedField.setRendererClass("TimestampFieldRenderer");
+        return dateCreatedField;
+    }
+
+    public static DataField createDateModifiedField() {
+        DataField dateModifiedField = new DataField("Date Modified", "number");
+        dateModifiedField.setUri("de/deepamehta/core/property/DateModified");
+        // dateModifiedField.setReadOnly(true);
+        dateModifiedField.setIndexingMode("FULLTEXT_KEY");
+        dateModifiedField.setRendererClass("TimestampFieldRenderer");
+        return dateModifiedField;
+    }
+
+    // ---------------------------------------------------------------------------------------------- Instance Variables
+
     private Logger logger = Logger.getLogger(getClass().getName());
 
 
@@ -26,33 +48,30 @@ public class TimePlugin extends Plugin {
 
     @Override
     public void preCreateHook(Topic topic, Map<String, String> clientContext) {
-        //
+        // 1) extend type definition
         if (topic.typeUri.equals("de/deepamehta/core/topictype/TopicType")) {
             // Add "Date Created" and "Date Modified" data fields to the topic type being created.
+            //
             // Note: Topic types created before the time plugin was activated get these fields through the initial
             // migration. See de.deepamehta.plugins.time.migrations.Migration1
             //
             // TODO: Avoid this code doubling by providing a "update type definition" facility.
             //
-            DataField dateCreatedField = new DataField("Date Created", "number");
-            dateCreatedField.setUri("de/deepamehta/core/property/DateCreated");
-            dateCreatedField.setIndexingMode("FULLTEXT_KEY");
-            dateCreatedField.setRendererClass("TimestampFieldRenderer");
-            //
-            DataField dateModifiedField = new DataField("Date Modified", "number");
-            dateModifiedField.setUri("de/deepamehta/core/property/DateModified");
-            dateModifiedField.setIndexingMode("FULLTEXT_KEY");
-            dateModifiedField.setRendererClass("TimestampFieldRenderer");
-            //
-            ((TopicType) topic).addDataField(dateCreatedField);
-            ((TopicType) topic).addDataField(dateModifiedField);
+            ((TopicType) topic).addDataField(createDateCreatedField());
+            ((TopicType) topic).addDataField(createDateModifiedField());
         }
         //
-        // add a timestamp to the topic being created
-        logger.info("Adding timestamp to " + topic);
+        // 2) set timestamp
+        logger.info("Setting timestamp of " + topic);
         long time = System.currentTimeMillis();
-        topic.setProperty("de/deepamehta/core/property/DateCreated", time);
-        topic.setProperty("de/deepamehta/core/property/DateModified", time);
+        TopicType type = dms.getTopicType(topic.typeUri);
+        // Note: the timestamp data fields might be (interactively) removed meanwhile
+        if (type.hasDataField("de/deepamehta/core/property/DateCreated")) {
+            topic.setProperty("de/deepamehta/core/property/DateCreated", time);
+        }
+        if (type.hasDataField("de/deepamehta/core/property/DateModified")) {
+            topic.setProperty("de/deepamehta/core/property/DateModified", time);
+        }
     }
 
     @Override
