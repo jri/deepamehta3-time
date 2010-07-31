4f3121a537c9c2bf980ead146236b45cc3c02510
Jörg Richter <jri@deepamehta.de>
2010-07-31 16:48:05 +0200

    Adapted to current version of DeepaMehta 3 Core.

--- src/main/resources/web/script/dm3_time.js
+++ src/main/resources/web/script/dm3_time.js
@@ -11,6 +11,8 @@ function dm3_time() {
 
 
 
+    /*** Provide "By Time" search mode ***/
+
     this.init = function() {
         $("#searchmode-select").append($("<option>").text("By Time"))
     }
@@ -26,18 +28,19 @@ function dm3_time() {
 
     this.search = function(searchmode) {
         if (searchmode == "By Time") {
-            // 1) perform time search
-            var mode_item = ui.menu_item("time_select")
+            // perform time search
+            var days = ui.menu_item("time_select").value
             var upper_date = new Date().getTime()
-            var lower_date = upper_date - mode_item.value * 24 * 60 * 60 * 1000
+            var lower_date = upper_date - days * 24 * 60 * 60 * 1000
             var query = "[" + lower_date + " TO " + upper_date + "]"
-            return dmc.search_topics(undefined, query, "de/deepamehta/core/property/DateModified", true)
+            return dmc.search_topics_and_create_bucket(query, "de/deepamehta/core/property/DateModified", true)
         }
     }
 
+    /*** Customizing search result view ***/
+
     this.render_topic_list_item = function(topic, list_item) {
         var timestamp = get_value(topic, "de/deepamehta/core/property/DateModified")
-        // alert(topic.properties.time_modified + " (" + typeof(topic.properties.time_modified) + ") => " + time)
         var time_div = $("<div>").addClass("result-item-time").append(format_timestamp(timestamp))
         return list_item.append(time_div)
     }
